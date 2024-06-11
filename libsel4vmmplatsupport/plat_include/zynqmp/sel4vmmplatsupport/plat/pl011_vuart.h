3b0698cf9d553d63e4768dbad158a36192a9e799
Eric Spidle <eric.spidle@dornerworks.com>
2024-06-11 06:34:29 -0700

    pl011_vuart: add zynqmp platform support
    Signed-off-by: Eric Spidle <eric.spidle@dornerworks.com>

--- libsel4vmmplatsupport/plat_include/zynqmp/sel4vmmplatsupport/plat/pl011_vuart.h
+++ libsel4vmmplatsupport/plat_include/zynqmp/sel4vmmplatsupport/plat/pl011_vuart.h
@@ -1,2 +1,16 @@
+/*
+ * Copyright 2024, DornerWorks
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
 #pragma once
 #include <sel4vmmplatsupport/arch/pl011_vuart.h>
+
+#define PL011_VUART_PLAT_SUPPORT
+#define PL011_VUART_ADDR       0xA000A000
+#define PL011_VUART_SIZE       0x1000
+#define PL011_VUART_SPEED      115200
+#define PL011_VUART_PORT       0x0
+#define PL011_VUART_IRQ_TYPE   0x0
+#define PL011_VUART_IRQ_NUM    0xA1
+#define PL011_VUART_IRQ_FLAGS  0x4
